4fcff1f89073529ffb785185858203bc28cddd58
kerkox <bryanpoolker@gmail.com>
2016-04-21 10:59:32 -0500

    v 0.0.7 Funciones auxiliares en la deteccion de los datos de los campos auto

--- src/xmltomysql/XML.java
+++ src/xmltomysql/XML.java
@@ -16,8 +16,8 @@ import org.jdom2.input.SAXBuilder;
  * @author paulker
  */
 public class XML {
-    
-    private File xmlFile; 
+
+    private File xmlFile;
     private File salida;
     private FileOutputStream fout;
     private BufferedOutputStream bout;
@@ -26,15 +26,16 @@ public class XML {
         this.xmlFile = xmlFile;
     }
 
-public void loadSalida() throws FileNotFoundException{
-    salida = new File("/home/paulker/SQL/Salida.txt");
-    fout = new FileOutputStream(salida);
-    bout = new BufferedOutputStream(fout);
-    
-    //Aqui va la estructura basica del codigo a convertir en SQL
-    String datasBasicos ="";
-    
-}    
+    public void loadSalida() throws FileNotFoundException {
+        salida = new File("/home/paulker/SQL/Salida.txt");
+        fout = new FileOutputStream(salida);
+        bout = new BufferedOutputStream(fout);
+
+        //Aqui va la estructura basica del codigo a convertir en SQL
+        String datasBasicos = "";
+
+    }
+
     public void cargarXML() {
         System.out.println("Comenzo a cargar el archivo XML");
         SAXBuilder builder = new SAXBuilder();
@@ -47,34 +48,59 @@ public void loadSalida() throws FileNotFoundException{
             Document document = (Document) builder.build(xmlFile);
 
             Element rootNode = document.getRootElement();
-            
-            List list = rootNode.getChildren("UnivalleBD");
-            System.out.println("Tamaño list: "+ list.size());
+
+            List list = rootNode.getChildren();
+            System.out.println("Tamaño list: " + list.size());
             Element tabla = (Element) list.get(0);
-                String nombreTabla = tabla.getName();
-                System.out.println("Tabla: " + nombreTabla);
+            String nombreTabla = tabla.getName();
+            System.out.println("Tabla: " + nombreTabla);
+
+            List lista_campos = tabla.getChildren();
+            String[] campos2;
+            String campos = "";
+            for (int j = 0; j < lista_campos.size(); j++) {
+//            for (int j = 0; j < 10; j++) {
+                Element campo = (Element) lista_campos.get(j);
+                campos += campo.getName() + "\t";
                 
-                List lista_campos = tabla.getChildren();
-                String campos="";
-                for (int j = 0; j < lista_campos.size(); j++) {
-                    Element campo = (Element) lista_campos.get(j);
-                    campos+=campo.getName()+"\t";
-                
-                }
-                System.out.println(campos);
-                
+            }
+            campos2 = campos.split("\t");
+            System.out.println(campos);
+            //Variable del tamaño de los campos
+            int size = lista_campos.size();
             for (int i = 0; i < list.size(); i++) {
                 tabla = (Element) list.get(i);
                 lista_campos = tabla.getChildren();
-                String datos="";
-                for (int j = 0; j < lista_campos.size(); j++) {
-                    Element campo = (Element) lista_campos.get(j);
-                    datos+=campo.getValue()+"\t";
+                String[] datos = new String[size];
+                getValuesCampos(campos2, datos, lista_campos);
+
+                /**
+                 * PENDIENTE: 
+                 * +Detectar automaticamente si es cadena o numero para el sql
+                 * +Pasar manualmente los tipos cadena o numero (longitud auto)
+                 * +Pasar manualmente los tipos detallando longitud
+                 */
+                
+                //Cambio especifico-----------------------------
+                datos[6] = datos[6].substring(5, datos[6].length());
+//                datos[6] = datos[6].replace("C.C. ", "");
+                String datas = ",(";
+                for (int x = 0; x < datos.length; x++) {
+                    if (x == 10) {
+                        datas += "'" + datos[x] + "'";
+                    } else if (x == 4 || x == 5 || x == 7 || x == 8 || x == 9) {
+                        datas += "'" + datos[x] + "'" + ",";
+                    } else {
+                        datas += datos[x] + ",";
+                    }
 
                 }
-
-                System.out.println(datos);
-                
+                datas += ")";
+                if (datos[9].contains("@")) {
+                    System.out.println("######+++++DATOS SUPRIMIDOS");
+                    System.out.println("Valor del ultimo: " + datos[10]);
+                }
+                System.out.println(datas);
             }
         } catch (IOException io) {
             System.out.println(io.getMessage());
@@ -85,10 +111,28 @@ public void loadSalida() throws FileNotFoundException{
         }
 
     }
-    
-    public void escribir(String data){
+
+    public void escribir(String data) {
         //Aqui escribimos el codigo para insertar un registro y este se convierte
 //        en el codigo necesario para sql
+    }
+    
+    
+    public void getValuesCampos(String[] camposName,String[] camposValue, List<Element> lista_campos){
+        int indexLista=0;
+        for(int x=0;x<camposName.length;x++){
+            Element campo = (Element) lista_campos.get(indexLista);
+            
+            if(camposName[x].equalsIgnoreCase(campo.getName())){
+                camposValue[x]=campo.getValue();
+                indexLista++;
+            }else{
+                camposValue[x]="0";
+            }
+            
+            
+        }
+        
     }
 
 }
